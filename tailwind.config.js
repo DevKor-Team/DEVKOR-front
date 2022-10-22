9afbd7467d4bb2090d2283443a2dd6e42d2c083e
Kunhee Kim <kunheekim@korea.ac.kr>
2022-10-22 15:03:29 +0900

    chore: package & config settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        button: "#222222",
+        devkor: "#F6B55A",
+      },
+    },
     fontFamily: {
       noto: ["Noto Sans KR"],
     },
