81bc44916743de3611fe51b2ece32bc563e75006
aiccuracy <soojin.emily@gmail.com>
2022-11-12 23:17:07 +0900

    refactor: make component thiner

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -37,7 +37,7 @@ const MyPage = () => {
 
         control: () => ({
             width: "100%",
-            height: "3.5rem",
+            height: "4rem",
             display: "flex",
             background: "#222222",
             padding: "0.5rem",
@@ -50,7 +50,7 @@ const MyPage = () => {
             alignItems: "center",
         }),
         singleValue: () => ({
-            fontSize: "1.05rem",
+            fontSize: "1.15rem",
             opacity: 0.6,
             letterSpacing: ".01rem",
             fontWeight: 400,
@@ -68,7 +68,7 @@ const MyPage = () => {
 
         control: () => ({
             width: "100%",
-            height: "3.5rem",
+            height: "4rem",
             display: "flex",
             background: "#222222",
             padding: "0.5rem",
@@ -81,7 +81,7 @@ const MyPage = () => {
             alignItems: "center",
         }),
         singleValue: () => ({
-            fontSize: "1.05rem",
+            fontSize: "1.15rem",
             opacity: 0.6,
             letterSpacing: ".01rem",
             fontWeight: 400,
@@ -99,82 +99,85 @@ const MyPage = () => {
 
     return (
         <div className="text-white">
-            <h2 className="text-xl font-semibold my-5 mx-10">마이페이지</h2>
+            <div className="text-xl font-semibold my-10 mx-[10%] flex justify-start">마이페이지</div>
             <form className="mx-10" onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex justify-center">
-                    <div className="bg-[#222222] w-[50%] rounded-lg mr-5 h-[25rem]">
-                        <h4 className="text-lg px-8 py-5">프로필</h4>
-                        <div className="flex flex-col items-center">
-                            <div className="mb-10 mt-5">
-                                <Image src="/images/ProfileDefault.jpg" alt="profile" width="120" height="120" className="rounded-full" />
+                {/* <div className="my-5 text-xl font-semibold">마이페이지</div> */}
+                <div className="flex justify-center h-[36rem]">
+                    <div className="bg-[#222222] w-[40%] rounded-lg mr-10 h-full">
+                        <h4 className="text-lg px-8 pt-5 h-[10%] ">프로필</h4>
+                        <div className="flex flex-col items-center h-[90%]">
+                            <div className="pt-8 h-[50%]">
+                                <Image src="/images/ProfileDefault.jpg" alt="profile" width="160" height="160" className="rounded-full" />
                             </div>
-                            <div className="flex flex-wrap justify-start w-[80%] items-end mb-5">
+                            <div className="flex flex-wrap justify-center w-[80%] items-end mb-5">
                                 <Tag name="FE" />
                                 <Tag name="UI/UX" />
                                 <Tag name="CV" />
-                                <Tag name="ML" />
+                                <Tag name="the DEVKOR" />
+                                <Tag name="the DEVKOR" />
                                 <Tag name="the DEVKOR" />
                             </div>
                         </div>
                     </div>
-                    <div className="w-[50%] ml-5 h-[25rem]">
-                        <div>
-                            <Select
-                                options={positionOptions}
-                                styles={customStyles}
-                                placeholder="주요 포지션"
-                                isClearable={false}
-                                isSearchable={false}
-                                menuPosition={"fixed"}
-                                theme={(theme) => ({
-                                    ...theme,
-                                    colors: {
-                                        ...theme.colors,
-                                        primary25: "#F6B55A",
-                                        primary: "black",
-                                    },
-                                })}
-                                onChange={(value) => {
-                                    setSelectedPosition(value?.value);
-                                }}
-                            />
+                    <div className="w-[40%] ml-10">
+                        <div className="h-[40%]">
+                            <div>
+                                <Select
+                                    options={positionOptions}
+                                    styles={customStyles}
+                                    placeholder="주요 포지션"
+                                    isClearable={false}
+                                    isSearchable={false}
+                                    menuPosition={"fixed"}
+                                    theme={(theme) => ({
+                                        ...theme,
+                                        colors: {
+                                            ...theme.colors,
+                                            primary25: "#F6B55A",
+                                            primary: "black",
+                                        },
+                                    })}
+                                    onChange={(value) => {
+                                        setSelectedPosition(value?.value);
+                                    }}
+                                />
+                            </div>
+                            <div className="mt-8">
+                                <Select
+                                    options={projectOptions}
+                                    styles={customStyles2}
+                                    placeholder="소속 스터디/프로젝트"
+                                    isClearable={false}
+                                    isSearchable={false}
+                                    theme={(theme) => ({
+                                        ...theme,
+                                        colors: {
+                                            ...theme.colors,
+                                            primary25: "#F6B55A",
+                                            primary: "black",
+                                        },
+                                    })}
+                                    onChange={(value) => {
+                                        setSelectedProject(value?.value);
+                                    }}
+                                />
+                            </div>
                         </div>
-                        <div className="mt-8">
-                            <Select
-                                options={projectOptions}
-                                styles={customStyles2}
-                                placeholder="소속 스터디/프로젝트"
-                                isClearable={false}
-                                isSearchable={false}
-                                theme={(theme) => ({
-                                    ...theme,
-                                    colors: {
-                                        ...theme.colors,
-                                        primary25: "#F6B55A",
-                                        primary: "black",
-                                    },
-                                })}
-                                onChange={(value) => {
-                                    setSelectedProject(value?.value);
-                                }}
-                            />
-                        </div>
-
-                        <div className="mt-5">
-                            <h2 className="">한줄 소개</h2>
-                            <textarea name="" id="" className="bg-[#222222] rounded-lg w-[100%] h-[10rem] mt-5 p-5"></textarea>
+                        <div className="h-[10%] tracking-wide p-1 text-lg">한줄 소개</div>
+                        <div className="h-[50%]">
+                            <textarea name="" id="" className="bg-[#222222] rounded-lg w-[100%] h-full p-5"></textarea>
                         </div>
                     </div>
                 </div>
-                <div className="flex">
-                    <div className="w-[50%] mr-5 flex flex-col my-10">
+                <div className="flex justify-center">
+                    <div className="w-[40%] mr-10 flex flex-col my-10">
                         <input type="text" placeholder="이름" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("name")} />
                         <input type="text" placeholder="학과" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("major")} />
                         <input type="text" placeholder="GitHub" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("github")} />
                         <input type="text" placeholder="Instagram" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("instagram")} />
                         <input type="text" placeholder="Blog" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("blog")} />
                     </div>
-                    <div className="w-[50%] ml-5 flex flex-col my-10">
+                    <div className="w-[40%] ml-10 flex flex-col my-10">
                         <input type="text" placeholder="생년월일" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("birthday")} />
                         <input type="text" placeholder="학번" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("studentNo")} />
                         <input type="text" placeholder="MBTI" className="bg-black border-b-2 border-[#222222] my-10 p-2 opacity-60" {...register("mbti")} />
