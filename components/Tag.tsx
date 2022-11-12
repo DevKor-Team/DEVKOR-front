0642cf626e64bbecf55fc26b1ba7e4aea05448e4
aiccuracy <soojin.emily@gmail.com>
2022-11-12 23:16:26 +0900

    refactor: change tag y-axis margin

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -6,7 +6,7 @@ interface TagProps {
 
 export const Tag = ({ name }: TagProps) => {
     return (
-        <div className="rounded-[1.2rem] bg-devkor flex h-9 justify-evenly items-center mx-1 px-4 my-2">
+        <div className="rounded-[1.2rem] bg-devkor flex h-9 justify-evenly items-center mx-1 px-4 my-3">
             <p className="text-md">{name}</p>
             <IoCloseOutline className="text-xl" />
         </div>
