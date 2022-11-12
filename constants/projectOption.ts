21fb8d9155fc6e6f5828456be79e4b39230452be
aiccuracy <soojin.emily@gmail.com>
2022-11-12 23:44:18 +0900

    fix: duplicated value between project and position

--- constants/projectOption.ts
+++ constants/projectOption.ts
@@ -1,9 +1,9 @@
 export const projectOptions = [
     { value: "", label: "소속 스터디/프로젝트" },
-    { value: "Frontend", label: "Frontend 스터디" },
-    { value: "Express", label: "Backend-Express 스터디" },
-    { value: "Nest", label: "Backend-Nest 스터디" },
-    { value: "ML", label: "ML 스터디" },
-    { value: "WebGL", label: "WebGL 스터디" },
-    { value: "MobileApp", label: "앱(Flutter) 스터디" },
+    { value: "Frontend Team", label: "Frontend 스터디" },
+    { value: "Express Team", label: "Backend-Express 스터디" },
+    { value: "Nest Team", label: "Backend-Nest 스터디" },
+    { value: "ML Team", label: "ML 스터디" },
+    { value: "WebGL Team", label: "WebGL 스터디" },
+    { value: "MobileApp Team", label: "앱(Flutter) 스터디" },
 ];
