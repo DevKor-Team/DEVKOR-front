715e3a7f7f2ad8173485038d034556c23d185ab5
aiccuracy <soojin.emily@gmail.com>
2022-11-15 23:57:48 +0900

    feat: add notification when copied on clipboard

---
+++ components/Alert.tsx
@@ -0,0 +1,7 @@
+export const Alert = () => {
+    return (
+        <div className="rounded-md text-white bg-devkor h-[3.5rem] flex justify-start items-center px-5">
+            <p className="">클립보드에 복사되었습니다</p>
+        </div>
+    );
+};
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,46 +4,60 @@ import { FiMail, FiInstagram } from "react-icons/fi";
 import { SiNotion } from "react-icons/si";
 import { useRouter } from "next/router";
 import { DevKorApply } from "../constants/devkor";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Alert } from "./Alert";
 
 export const Footer = () => {
     const router = useRouter();
     const [copied, setCopied] = useState<boolean>(false);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCopied(false);
+        }, 4000);
+    }, [copied]);
+
     return (
-        <div className="border-t border-devkor text-devkor px-[9.375rem] pb-5">
-            <div>
-                <Image src="/images/devkor-logo.svg" alt="devkor-logo" width="100" height="100" />
+        <div className="border-t border-devkor text-devkor px-[9.375rem] pb-5 flex relative">
+            <div className="w-[70%]">
+                <div>
+                    <Image src="/images/devkor-logo.svg" alt="devkor-logo" width="100" height="100" />
+                </div>
+                <h4>문의</h4>
+                <div className="flex pt-5">
+                    <RiKakaoTalkFill
+                        className="mr-[0.6rem] text-2xl cursor-pointer"
+                        onClick={() => {
+                            router.push(DevKorApply.kakao);
+                        }}
+                    />
+                    <FiMail
+                        className="mx-[0.6rem] text-2xl cursor-pointer"
+                        onClick={() => {
+                            navigator.clipboard.writeText(DevKorApply.email);
+                            setCopied(true);
+                        }}
+                    />
+                    <SiNotion
+                        className="mx-[0.6rem] text-2xl cursor-pointer"
+                        onClick={() => {
+                            router.push(DevKorApply.notion);
+                        }}
+                    />
+                    <FiInstagram
+                        className="ml-[0.6rem] text-2xl cursor-pointer"
+                        onClick={() => {
+                            router.push(DevKorApply.instagram);
+                        }}
+                    />
+                </div>
+                <div className="border-b border-devkor mt-5 w-[30%]"></div>
+                <p className="text-sm font-light mt-3">Copyright ⓒ DevKor. All Rights Reserved.</p>
             </div>
-            <h4>문의</h4>
-            <div className="flex pt-5">
-                <RiKakaoTalkFill
-                    className="mr-[0.6rem] text-2xl cursor-pointer"
-                    onClick={() => {
-                        router.push(DevKorApply.kakao);
-                    }}
-                />
-                <FiMail
-                    className="mx-[0.6rem] text-2xl cursor-pointer"
-                    onClick={() => {
-                        navigator.clipboard.writeText(DevKorApply.email);
-                        setCopied(true);
-                    }}
-                />
-                <SiNotion
-                    className="mx-[0.6rem] text-2xl cursor-pointer"
-                    onClick={() => {
-                        router.push(DevKorApply.notion);
-                    }}
-                />
-                <FiInstagram
-                    className="ml-[0.6rem] text-2xl cursor-pointer"
-                    onClick={() => {
-                        router.push(DevKorApply.instagram);
-                    }}
-                />
+
+            <div className={"w-[20%] absolute bottom-5 right-3 transition-all duration-500 " + (copied ? "opacity-100" : "opacity-0")}>
+                <Alert />
             </div>
-            <div className="border-b border-devkor mt-5 w-[30%]"></div>
-            <p className="text-sm font-light mt-3">Copyright ⓒ DevKor. All Rights Reserved.</p>
         </div>
     );
 };
