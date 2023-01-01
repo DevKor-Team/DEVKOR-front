53453ecec9057eb653e809eb48e4fd2fdb966d2f
Soojin An <87556025+aiccuracy@users.noreply.github.com>
2023-01-01 19:31:44 +0900

    feat: add footer (#8)
    * feat: add footer
    
    * feat: add notification when copied on clipboard

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import { Footer } from "./Footer";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="bg-black w-full">
             <Header />
             <main className="px-[9.375rem]">{children}</main>
+            <Footer />
         </div>
     );
 };
