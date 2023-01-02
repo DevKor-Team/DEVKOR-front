f1258c0803dbaf143449787f4c3ea91eed939598
Seungwoo Lee <timosean@naver.com>
2023-01-02 15:42:58 +0900

    feat: add tagbutton to postview

--- pages/tech/post/[postID].tsx
+++ pages/tech/post/[postID].tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { FiHeart, FiDownload } from "react-icons/fi";
+import { IoChatbox } from "react-icons/io";
+import { TagButton } from "../../../components/Tags";
 
 const PostView: NextPage = () => {
   const router = useRouter();
   const { postID } = router.query;
 
-  return <div>포스트뷰</div>;
+  return (
+    <div className="flex justify-center">
+      <section className="w-11/12 mt-24 flex flex-col justify-center">
+        <div className="flex justify-between">
+          <TagButton name="FrontEnd" />
+        </div>
+        <h1 className="text-5xl font-bold">Next.js의 Hydration 파헤치기</h1>
+      </section>
+    </div>
+  );
 };
 
 export default PostView;
