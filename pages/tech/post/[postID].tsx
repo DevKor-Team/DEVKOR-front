054861414182cea0834b955a2ca9e39dabefa0f6
Seungwoo Lee <timosean@naver.com>
2023-01-02 16:10:30 +0900

    feat: add Icons to Postview page

--- pages/tech/post/[postID].tsx
+++ pages/tech/post/[postID].tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { FiHeart, FiDownload } from "react-icons/fi";
-import { IoChatbox } from "react-icons/io";
-import { TagButton } from "../../../components/Tags";
+import { FaHeart } from "react-icons/fa"; //꽉 찬 하트
+import { IoChatbox } from "react-icons/io5";
+import { PostTag } from "../../../components/Tags";
 
 const PostView: NextPage = () => {
   const router = useRouter();
@@ -10,11 +11,30 @@ const PostView: NextPage = () => {
 
   return (
     <div className="flex justify-center">
-      <section className="w-11/12 mt-24 flex flex-col justify-center">
+      <section className="w-9/12 mt-24 flex flex-col justify-center">
         <div className="flex justify-between">
-          <TagButton name="FrontEnd" />
+          <PostTag category="FrontEnd" />
+          <div className="flex items-center space-x-4">
+            <FiHeart
+              stroke="#F6B55A"
+              size="24"
+              className="hover:cursor-pointer"
+            />
+            <FiDownload
+              stroke="#F6B55A"
+              size="24"
+              className="hover:cursor-pointer"
+            />
+            <IoChatbox
+              fill="#F6B55A"
+              size="24"
+              className="hover:cursor-pointer"
+            />
+          </div>
         </div>
-        <h1 className="text-5xl font-bold">Next.js의 Hydration 파헤치기</h1>
+        <h1 className="text-5xl font-bold mt-4">
+          Next.js의 Hydration 파헤치기
+        </h1>
       </section>
     </div>
   );
