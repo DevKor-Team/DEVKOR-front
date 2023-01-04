db8ddb9e1b72a1294236b42d3add060d9943068e
Seungwoo Lee <timosean@naver.com>
2023-01-04 15:50:26 +0900

    feat: 모바일 레이아웃 좌우패딩 수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Header from "./Header";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className="bg-black w-full">
-            <Header />
-            <main className="px-[9.375rem]">{children}</main>
-        </div>
-    );
+  return (
+    <div className="bg-black w-full">
+      <Header />
+      <main className="px-[3rem] md:px-[9.375rem]">{children}</main>
+    </div>
+  );
 };
