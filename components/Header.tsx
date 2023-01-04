329a9d3a977bc7a7a1da3b0c5f1ba9c792fedb9b
Seungwoo Lee <timosean@naver.com>
2023-01-04 15:50:37 +0900

    feat: 모바일 햄버거 메뉴 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { SingleMenu, MenuList } from "./HeaderItem";
 import Link from "next/link";
+import { useState } from "react";
+import Hamburger from "hamburger-react";
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
-    <header className="w-full px-[9.375rem] flex bg-black justify-between items-center h-[5rem]">
+    <header className="w-full px-[3rem] md:px-[9.375rem] flex bg-black justify-between items-center h-[5rem]">
       <section className="flex">
         {/* 헤더 로고 */}
         <div className="flex items-center pr-[2rem] w-[6.75rem] h-[5.625rem] hover:cursor-pointer">
@@ -24,15 +27,25 @@ const Header = () => {
           ))}
         </div>
       </section>
-      {/* 마이페이지 아이콘 */}
+      {/* 마이페이지 아이콘 or 햄버거(모바일)*/}
       <section className="py-[1.875rem] flex items-center cursor-pointer">
-        <Image
-          src="/images/profile-logo.png"
-          alt="profile"
-          width="25"
-          height="25"
-          layout="fixed"
-        />
+        <div className="hidden md:flex">
+          <Image
+            src="/images/profile-logo.png"
+            alt="profile"
+            width="25"
+            height="25"
+            layout="fixed"
+          />
+        </div>
+        <div className="flex md:hidden">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            size={24}
+            color={"#F6B55A"}
+          />
+        </div>
       </section>
     </header>
   );
