4f95d40936deb0159aaea78247b8fce2983ebfd7
Seungwoo Lee <timosean@naver.com>
2023-01-04 12:20:54 +0900

    feat: add link to home when clicking header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import { SingleMenu } from "./HeaderItem";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="w-full flex bg-black justify-between items-center h-[5rem]">
       <section className="flex pl-[9.375rem]">
-        <div className="flex items-center pr-[2rem] w-[6.75rem] h-[5.625rem]">
-          <Image
-            src="/images/devkor-logo.svg"
-            alt="devkor"
-            width="108"
-            height="90"
-          ></Image>
+        <div className="flex items-center pr-[2rem] w-[6.75rem] h-[5.625rem] hover:cursor-pointer">
+          <Link href="/">
+            <Image
+              src="/images/devkor-logo.svg"
+              alt="devkor"
+              width="108"
+              height="90"
+            ></Image>
+          </Link>
         </div>
         <div className="flex items-center py-[1.875rem]">
           <SingleMenu title="Tech" />
